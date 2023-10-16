02644807354668ef5295b93978a947808c3046e2
Cavon Lee <cavonxx@gmail.com>
2023-10-16 15:38:58 +0800

    remove smbus from requirement

--- setup.py
+++ setup.py
@@ -66,5 +66,5 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['RPi.GPIO', 'smbus'],
+    install_requires=['RPi.GPIO'],
 )
